ee9ed1dd376d9cdf17d303c5c1bab88d2ef13167
jimi von Burg <rapiddude@hotmail.com>
2021-04-13 10:57:52 +1000

    Adding DB strings for Heroku to contexts

--- workoutAPI/Models/ExerciseContext.cs
+++ workoutAPI/Models/ExerciseContext.cs
@@ -12,8 +12,22 @@ namespace WorkoutAPI.Models
         {
         }
 
+        private string GetHerokuConnectionString()
+        {
+            // Get the connection string from the ENV variables
+            string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            // parse the connection string
+            var databaseUri = new Uri(connectionUrl);
+
+            string db = databaseUri.LocalPath.TrimStart('/');
+            string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseNpgsql("Host=localhost;Database=workout;Username=postgres;Password=", optionsBuilder => optionsBuilder.SetPostgresVersion(new Version(9, 6)));
+    => optionsBuilder.UseNpgsql(GetHerokuConnectionString(), optionsBuilder => optionsBuilder.SetPostgresVersion(new Version(9, 6)));
 
         public DbSet<Exercise> exercise { get; set; }
     }
